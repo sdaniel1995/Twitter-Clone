4f5a2de9a9c3c79b77af7999452f45e7e578ff90
Shane <danielshane861@gmail.com>
2021-05-21 01:42:24 -0400

    Cleaned file structure

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 
 const Counter = () => {
     const [count, setCounter] = useState(0)
+    
     return (
         <div>
             <button onMouseOver={() => {setCounter(count+1)}}>button pressed {count} times </button>
